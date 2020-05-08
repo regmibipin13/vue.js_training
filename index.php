e0a115417486144b4e860f3a316f4e6436003c9b
Bipin Regmi <bipinregmi@Bipins-MacBook-Air.local>
2020-05-08 10:39:30 +0545

    First Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vue js Training</title>
+</head>
+<body>
+<div id="app">
+	{{ product }}
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script type="text/javascript" src="vue.js"></script>
+</body>
+</html>
